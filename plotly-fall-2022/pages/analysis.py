7fa4168ff3e55611e77d62b59100abd799ef5fe7
Julie Hartz <julie.hartz13@gmail.com>
2022-09-12 22:21:49 -0400

    finishes coding the third tab of analysis page

--- plotly-fall-2022/pages/analysis.py
+++ plotly-fall-2022/pages/analysis.py
@@ -39,7 +39,7 @@ layout = html.Div([
                         ],
                         style = {'margin-top': '0.5vh', 'margin-left': '0.1vw'},
                         id="card-tabs",
-                        active_tab="county",
+                        active_tab="specific",
                     )
                 ),
                 dbc.CardBody(html.P(id="card-content", className= "card-text")),
@@ -56,6 +56,7 @@ layout = html.Div([
 def render_tab_content(tab_value, data):
     df = pd.DataFrame(data)
     county_options = [{'label': i, 'value': i} for i in df['county'].unique()]
+    store_options = [{'label': i, 'value': i} for i in df['store_number'].unique()]
     if tab_value == 'iowa':
         return html.Div([
             html.Div(id = 'iowa-store', style = {'width': '88vw', 'height': '67vh', 'backgroundColor': 'rgba(164, 229, 122, 0.5)', 'border-radius': '5px'})
@@ -85,7 +86,25 @@ def render_tab_content(tab_value, data):
             ])
     else:
         return html.Div([
-            html.Div(id = 'specific-store', style = {'width': '88vw', 'height': '67vh', 'backgroundColor': 'rgba(164, 229, 122, 0.5)', 'border-radius': '5px'})
+            html.Div(id = 'specific-store', children = [
+                html.Div([
+                    html.Div(id = 'specific-store-1', children = [
+                        dcc.Dropdown(id = 'specific-dropdown-1', options = store_options, value = store_options[0]['value'], style = {'order':'1', 'width': '30vw', 'margin-left': '0.8rem', 'margin-top': '0.5rem'}),
+                        html.Div(id = 'specific-store-1-content', style = {'order':'2', 'width': '41vw', 'height': '57vh', 'border-radius': '5px', 'margin-left': '0.5rem', 'margin-top': '0.5rem'})
+                    ],
+                    style = {'order': '1', 'width': '42vw', 'height': '65vh', 'margin': 'auto',
+                             'display': 'flex', 'flex-direction': 'column', 'align-items': 'flex-start',
+                             'backgroundColor': 'white', 'border-radius': '5px', 'margin-top': '0.5rem'}),
+                    html.Div(id = 'specific-store-2', children = [
+                        dcc.Dropdown(id = 'specific-dropdown-2', options = store_options, value = store_options[0]['value'], style = {'width': '30vw', 'margin-left': '0.8rem', 'margin-top': '0.5rem'}),
+                        html.Div(id = 'specific-store-2-content', style = {'order':'2', 'width': '41vw', 'height': '57vh', 'border-radius': '5px', 'margin-left': '0.5rem', 'margin-top': '0.5rem'})
+                    ],
+                    style = {'order': '2', 'width': '42vw', 'height': '65vh', 'margin': 'auto',
+                             'display': 'flex', 'flex-direction': 'column', 'align-items': 'flex-start',
+                             'backgroundColor': 'white', 'border-radius': '5px', 'margin-top': '0.5rem'})
+                    ], style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'margin': 'auto',
+                                'justify-content': 'space-between', 'width': '88vw', 'height': '65vh'})
+            ], style = {'width': '88vw', 'height': '67vh', 'backgroundColor': 'rgba(164, 229, 122, 0.5)', 'border-radius': '5px'})
             ])
 
 @callback(Output('iowa-store', 'children'),
@@ -171,6 +190,44 @@ def render_county_store_1(county1, county2, data):
             ], style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'margin': 'auto',
                         'justify-content': 'space-between', 'width': '88vw', 'height': '62vh'})
 
+@callback(Output('specific-store-1-content', 'children'),
+         Input('specific-dropdown-1', 'value'),
+         Input('specific-dropdown-2', 'value'),
+         Input('store-data', 'data')
+)
+def render_county_store_1(store1, store2, data):
+    df = pd.DataFrame(data)
+    store_name_1 = df[df['store_number']== store1]['store_name'].unique()[0]
+    store_name_2 = df[df['store_number']== store2]['store_name'].unique()[0]
+    return html.Div([
+            dcc.Store(id = 'specific-store-infos', data = {'store_name_1': store_name_1, 'store_number_1': store1, 'store_name_2': store_name_2, 'store_number_2': store2}),
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.H3(f'{store_name_1}', style = {'order':'1', 'color': 'black', 'text-align': 'left', 'margin-top': '1rem', 'margin-left': '1rem'}),
+                        html.H5(f'Store number: {store1}', style = {'order':'2', 'color': 'black', 'text-align': 'left', 'margin-top': '0.5rem', 'margin-left': '1rem'}),
+                        radioitems,
+                    ], style = {'order':'1', 'display': 'flex', 'flex-direction': 'column', 'align-items': 'flex-start', 'width': '22vw', 'height': '20vh', 'margin-right': '2rem'}),
+                    dcc.Graph(id = 'dist-plot-5', style = {'order': '2', 'margin-bottom': '1rem', 'margin-right': '1rem'})
+                ], style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'width': '42vw', 'height': '20vh', 'margin-left': '0rem'}),
+                dcc.Graph(id = 'specific-1-cum', style = {'margin-left': '1rem', 'margin-top': '1rem'})
+                ], style = {'order': '1', 'width': '42vw', 'height': '60vh', 'margin': 'auto', 'margin-left': '0rem',
+                            'border-radius': '5px', 'margin-top': '-0.5rem'}),
+            html.Div([
+                html.Div([
+                    html.Div([
+                        html.H3(f'{store_name_2}', style = {'order':'1', 'color': 'black', 'text-align': 'left', 'margin-top': '1rem', 'margin-left': '1rem'}),
+                        html.H5(f'Store number: {store2}', style = {'order':'2', 'color': 'black', 'text-align': 'left', 'margin-top': '0.5rem', 'margin-left': '1rem'}),
+                        radioitems,
+                    ], style = {'order':'1', 'display': 'flex', 'flex-direction': 'column', 'align-items': 'flex-start', 'width': '22vw', 'height': '20vh', 'margin-right': '2rem', 'z-index': '1000'}),
+                    dcc.Graph(id = 'dist-plot-6', style = {'order': '2', 'margin-bottom': '1rem', 'margin-right': '1rem'})
+                ], style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'width': '42vw', 'height': '20vh', 'margin-left': '0rem'}),
+                dcc.Graph(id = 'specific-2-cum', style = {'margin-left': '1rem', 'margin-top': '1rem'})
+            ], style = {'order': '2', 'width': '42vw', 'height': '60vh', 'margin': 'auto', 'margin-left': '0rem',
+                        'border-radius': '5px', 'margin-top': '-0.5rem'})
+            ], style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', 'margin': 'auto',
+                        'justify-content': 'space-between', 'width': '88vw', 'height': '62vh'})
+
 @callback(Output('county-store-1-cum', 'figure'),
           Input('county-dropdown-1', 'value'),
           Input('store-data', 'data'),
@@ -239,6 +296,38 @@ def render_cum_graph(data, store_data):
     fig = plots.cum_sales(daily_sales)
     return fig
 
+@callback(Output('specific-1-cum', 'figure'),
+          Input('store-data', 'data'),
+          Input('specific-dropdown-1', 'value')
+)
+def render_cum_graph(data, store_number):
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date'])
+    df = df[df['store_number'] == store_number].sort_values('date', ascending=True).set_index('date')
+    daily_sales = df.groupby(pd.Grouper(freq="D")).sum()[['benefit', 'cost', 'profit']]
+    daily_sales['cum_sum_benefit'] = daily_sales['benefit'].cumsum()
+    daily_sales['cum_sum_cost'] = daily_sales['cost'].cumsum()
+    daily_sales['cum_sum_profit'] = daily_sales['profit'].cumsum()
+    daily_sales = daily_sales.reset_index()
+    fig = plots.cum_sales(daily_sales, county=True)
+    return fig
+
+@callback(Output('specific-2-cum', 'figure'),
+          Input('store-data', 'data'),
+          Input('specific-dropdown-2', 'value')
+)
+def render_cum_graph(data, store_number):
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date'])
+    df = df[df['store_number'] == store_number].sort_values('date', ascending=True).set_index('date')
+    daily_sales = df.groupby(pd.Grouper(freq="D")).sum()[['benefit', 'cost', 'profit']]
+    daily_sales['cum_sum_benefit'] = daily_sales['benefit'].cumsum()
+    daily_sales['cum_sum_cost'] = daily_sales['cost'].cumsum()
+    daily_sales['cum_sum_profit'] = daily_sales['profit'].cumsum()
+    daily_sales = daily_sales.reset_index()
+    fig = plots.cum_sales(daily_sales, county=True)
+    return fig
+
 @callback(Output('dist-plot-1', 'figure'),
           Input('store-data', 'data'),
             Input('store-infos', 'data')
@@ -288,5 +377,29 @@ def render_cum_graph(county, data, store_data):
     df['date'] = pd.to_datetime(df['date'])
     df_ = df.groupby(['store_number', 'store_name'], as_index=False).sum()[['store_number', 'store_name', 'profit']]
     store_profit = df_[df_['store_number'] == store_data['store_number_2']]['profit'].values[0]
+    fig = plots.dist_plot(df_, store_profit)
+    return fig
+
+@callback(Output('dist-plot-5', 'figure'),
+          Input('specific-dropdown-1', 'value'),
+          Input('store-data', 'data')
+)
+def render_cum_graph(store, data):
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date'])
+    df_ = df.groupby(['store_number', 'store_name'], as_index=False).sum()[['store_number', 'store_name', 'profit']]
+    store_profit = df_[df_['store_number'] == store]['profit'].sum()
+    fig = plots.dist_plot(df_, store_profit)
+    return fig
+
+@callback(Output('dist-plot-6', 'figure'),
+          Input('specific-dropdown-2', 'value'),
+          Input('store-data', 'data')
+)
+def render_cum_graph(store, data):
+    df = pd.DataFrame(data)
+    df['date'] = pd.to_datetime(df['date'])
+    df_ = df.groupby(['store_number', 'store_name'], as_index=False).sum()[['store_number', 'store_name', 'profit']]
+    store_profit = df_[df_['store_number'] == store]['profit'].sum()
     fig = plots.dist_plot(df_, store_profit)
     return fig
